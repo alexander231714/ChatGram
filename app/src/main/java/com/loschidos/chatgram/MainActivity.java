6c0a1048b186a1509f14b6b70306037c3ff939d9
vladi <vladi@LAPTOP-MQML25RF>
2023-04-13 16:19:40 -0600

    Cambios de apariencia en los layout de login, agrege alguna libreria circule y modifique los .java para hacer los Itent y un metodo login solo use los get

--- app/src/main/java/com/loschidos/chatgram/MainActivity.java
+++ app/src/main/java/com/loschidos/chatgram/MainActivity.java
@@ -4,20 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 import com.loschidos.chatgram.R;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mTextViewRegister;
+    TextInputEditText mTextInpuTEmail, mTextInputPassword;
+    Button mButtonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextInpuTEmail=findViewById(R.id.textInputEmail);
+        mTextInputPassword=findViewById(R.id.textInputPassword);
+        mButtonLogin=findViewById(R.id.btnLogin);
+
+
         mTextViewRegister = findViewById(R.id.textViewRegister);
+
+        //Button
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+
         mTextViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,5 +47,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void login() {
+        String email = mTextInpuTEmail.getText().toString();
+        String password = mTextInputPassword.getText().toString();
+        Log.d("CAMPO", "email: "+email);
+        Log.d("CAMPO", "password: "+password);
     }
 }
--- app/src/main/java/com/loschidos/chatgram/RegisterActivity2.java
+++ app/src/main/java/com/loschidos/chatgram/RegisterActivity2.java
@@ -3,12 +3,23 @@ package com.loschidos.chatgram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity2 extends AppCompatActivity {
 
+    CircleImageView mCirculeImageViewBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        mCirculeImageViewBack=findViewById(R.id.circleImageBack);
+        mCirculeImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
