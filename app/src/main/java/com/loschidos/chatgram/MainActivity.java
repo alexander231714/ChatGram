e89489fd63817c78ff5acf83b6ef9b3ef78b3df0
vladi <vladi@LAPTOP-MQML25RF>
2023-04-13 13:39:05 -0600

    Initial commit

---
+++ app/src/main/java/com/loschidos/chatgram/MainActivity.java
@@ -0,0 +1,30 @@
+package com.loschidos.chatgram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.loschidos.chatgram.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextViewRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewRegister = findViewById(R.id.textViewRegister);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
