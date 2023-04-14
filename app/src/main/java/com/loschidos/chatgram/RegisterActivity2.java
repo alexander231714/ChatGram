3e7a7e7fe09a77c89c0eeb1afbf22a45427503f3
vladi <vladi@LAPTOP-MQML25RF>
2023-04-13 22:35:03 -0600

    Hice la validacion del registro de  datos ya sea que el email sea valido en el RegisteActivity

--- app/src/main/java/com/loschidos/chatgram/RegisterActivity2.java
+++ app/src/main/java/com/loschidos/chatgram/RegisterActivity2.java
@@ -3,18 +3,44 @@ package com.loschidos.chatgram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity2 extends AppCompatActivity {
 
     CircleImageView mCirculeImageViewBack;
+    TextInputEditText mTextInputUsername, mTextInputEmail, mTextInputPassword, mTextInputConfirPassword;
+    Button mButtonRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+        mTextInputEmail=findViewById(R.id.textInputEmail);
+        mTextInputUsername=findViewById(R.id.textInputUsername);
+        mTextInputPassword=findViewById(R.id.textInputPassword);
+        mTextInputConfirPassword=findViewById(R.id.textInputConfirPassword);
+        mButtonRegister=findViewById(R.id.btnRegister);
+
         mCirculeImageViewBack=findViewById(R.id.circleImageBack);
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
+
         mCirculeImageViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,4 +48,31 @@ public class RegisterActivity2 extends AppCompatActivity {
             }
         });
     }
+
+    private void register() {
+        String username=mTextInputUsername.getText().toString();
+        String email=mTextInputEmail.getText().toString();
+        String password=mTextInputPassword.getText().toString();
+        String confirmPassword=mTextInputConfirPassword.getText().toString();
+
+        if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPassword.isEmpty()) {
+            if (isEmailValid(email)) {
+                Toast.makeText(this, "Has insertado todos los campos y el correo electrónico es válido", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "El correo electrónico no es válido, por favor ingresa uno válido", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Para continuar ingresa todos los campos", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    //Verifica que el correo ingresado sea valido
+    public boolean isEmailValid(String email) {
+        String expression = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
