61dd11efb37630e235ba16292b80668560e6d690
vladi <vladi@LAPTOP-MQML25RF>
2023-04-14 01:09:37 -0600

    Login funcionando, agarra correo y contrase;a y se pasa home

--- app/src/main/java/com/loschidos/chatgram/MainActivity.java
+++ app/src/main/java/com/loschidos/chatgram/MainActivity.java
@@ -1,5 +1,6 @@
 package com.loschidos.chatgram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.loschidos.chatgram.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     TextView mTextViewRegister;
     TextInputEditText mTextInpuTEmail, mTextInputPassword;
     Button mButtonLogin;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         mTextViewRegister = findViewById(R.id.textViewRegister);
 
+        //Inicializamos la autenticacion de firebase
+        mAuth = FirebaseAuth.getInstance();
         //Button
-        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 login();
@@ -52,6 +63,17 @@ public class MainActivity extends AppCompatActivity {
     private void login() {
         String email = mTextInpuTEmail.getText().toString();
         String password = mTextInputPassword.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(MainActivity.this, "Email o contraseña que ingresaste es incorrecta", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
         Log.d("CAMPO", "email: "+email);
         Log.d("CAMPO", "password: "+password);
     }
