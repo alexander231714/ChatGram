6c954ef8a96000e0099e8d0ba6795d4c556a6fc1
DavidM839 <107301911+DavidM839@users.noreply.github.com>
2023-04-16 19:01:04 -0600

    mejora de implementacion en el codigo de imagen

--- app/src/main/java/com/loschidos/chatgram/activities/PostActivity.java
+++ app/src/main/java/com/loschidos/chatgram/activities/PostActivity.java
@@ -1,8 +1,13 @@
 package com.loschidos.chatgram.activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -35,27 +40,31 @@ public class PostActivity extends AppCompatActivity {
             }
         });
     }
-
+    //Con este metodo se ejecuta cuando el usuario realiza la accion que lo lleva a la galeria
+ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(),
+        new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+     // Si la accion que realiza el usuario es la de seleccionar desde galeria caso contrario se mostrara un error
+             if(result.getResultCode() == Activity.RESULT_OK){
+            try {
+        imgFile = FileUtil.from(PostActivity.this,result.getData().getData());
+        imgpost1.setImageBitmap(BitmapFactory.decodeFile(imgFile.getAbsolutePath()));
+    }catch (Exception e){
+        Log.d("ERROR", "Se produjo un error al seleccionar la imagen" +e.getMessage());
+        Toast.makeText(PostActivity.this,"Se produjo un error"+e.getMessage(),Toast.LENGTH_LONG).show();
+    }
+             }
+            }
+        }
+);
     private void OpenGallery() {
         Intent galleryIntent = new Intent (Intent.ACTION_GET_CONTENT);
         galleryIntent.setType("image/*");
-        startActivityForResult(galleryIntent,GALLERY_REQUEST_CODE); //de esta forma se abre la galeria y se usa el numero como
+        galleryLauncher.launch(galleryIntent);
+      //  startActivityForResult(galleryIntent,GALLERY_REQUEST_CODE); (LINEA ALTERNA DE CODIGO)
+        //  de esta forma se abre la galeria y se usa el numero como
         // Un identificador  de la actividad que realiza en base a la Accion del usuario
-    }
-
-    //Con este metodo se ejecuta cuando el usuario realiza la accion que lo lleva a la galeria
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // Si la accion que realiza el usuario es la de seleccionar desde galeria caso contrario se mostrara un error
-        if(requestCode==GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
-          try {
-              imgFile = FileUtil.from(this,data.getData());
-              imgpost1.setImageBitmap(BitmapFactory.decodeFile(imgFile.getAbsolutePath()));
-          }catch (Exception e){
-              Log.d("ERROR", "Se produjo un error al seleccionar la imagen" +e.getMessage());
-              Toast.makeText(this,"Se produjo un error"+e.getMessage(),Toast.LENGTH_LONG).show();
-          }
-        }
     }
 }
