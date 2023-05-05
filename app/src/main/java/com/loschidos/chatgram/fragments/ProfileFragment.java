744fb8ec494225d635a9b68d148f08bac07d7186
DavidM839 <107301911+DavidM839@users.noreply.github.com>
2023-05-05 02:00:00 -0600

    ultimos detalles al apartado de perfil visualizar datos del perfil y conteo de publicaciones del usuario

--- app/src/main/java/com/loschidos/chatgram/fragments/ProfileFragment.java
+++ app/src/main/java/com/loschidos/chatgram/fragments/ProfileFragment.java
@@ -9,10 +9,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.loschidos.chatgram.R;
 import com.loschidos.chatgram.activities.EditProfileActivity;
+import com.loschidos.chatgram.providers.AuthProvider;
+import com.loschidos.chatgram.providers.PostProvider;
+import com.loschidos.chatgram.providers.UserProvider;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +31,18 @@ import com.loschidos.chatgram.activities.EditProfileActivity;
 public class ProfileFragment extends Fragment {
     LinearLayout mLinerLayoutEditProfile;
     View mView;
+    UserProvider mUserProvider;
+    AuthProvider mAuthProvider;
+
+    TextView mTextViewUserName;
+    TextView mTextViewPhone;
+    TextView mTextViewEmail;
+    TextView mTextViewPost;
+    ImageView mImageCover;
+    CircleImageView mImageProfile;
+
+    PostProvider mPostProvider;
+
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -32,8 +55,19 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_profile,container, false);
         mLinerLayoutEditProfile = mView.findViewById(R.id.linerLayoutEditProfile);
+        mTextViewEmail = mView.findViewById(R.id.TextViewEmail);
+        mTextViewUserName = mView.findViewById(R.id.TextViewUsername);
+        mTextViewPhone = mView.findViewById(R.id.TextViewPhone);
+        mTextViewPost = mView.findViewById(R.id.TextViewPost);
+        mImageProfile = mView.findViewById(R.id.CircleImageProfile);
+        mImageCover = mView.findViewById(R.id.ImageViewCover);
 
         mLinerLayoutEditProfile.setOnClickListener(view -> goToEditProfile());
+        mPostProvider =new PostProvider();
+        mUserProvider = new UserProvider();
+        mAuthProvider = new AuthProvider();
+        getUser();
+        getPostNumber();
         return mView;
     }
 
@@ -41,5 +75,66 @@ public class ProfileFragment extends Fragment {
        Intent intent = new Intent(getContext(), EditProfileActivity.class);
        startActivity(intent);
        Log.d("DEBUG", "Actividad EditProfileActivity iniciada");
+   }
+
+   private void getPostNumber(){
+mPostProvider.getPostByUser(mAuthProvider.getUid()).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+    @Override
+    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+        int PostNum = queryDocumentSnapshots.size();
+        mTextViewPost.setText(String.valueOf(PostNum));
+    }
+});
+   }
+
+   private void getUser(){
+    mUserProvider.getUser(mAuthProvider.getUid()).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        @Override
+        public void onSuccess(DocumentSnapshot documentSnapshot) {
+            if(documentSnapshot.exists()){
+                if(documentSnapshot.contains("email")){
+                    String email = documentSnapshot.getString("email");
+                    mTextViewEmail.setText(email);
+                }
+            }
+
+            if(documentSnapshot.exists()){
+                if(documentSnapshot.contains("phone")){
+                    String phone = documentSnapshot.getString("phone");
+                    mTextViewPhone.setText(phone);
+                }
+            }
+
+            if(documentSnapshot.exists()){
+                if(documentSnapshot.contains("username")){
+                    String username = documentSnapshot.getString("username");
+                    mTextViewUserName.setText(username);
+                }
+            }
+
+            if(documentSnapshot.exists()){
+                if(documentSnapshot.contains("image_profile")){
+                    String imageProfile = documentSnapshot.getString("image_profile");
+                    if(imageProfile != null){
+                        if(!imageProfile.isEmpty()){
+                            Picasso.with(getContext()).load(imageProfile).into(mImageProfile);
+                        }
+                    }
+                }
+            }
+
+            if(documentSnapshot.exists()){
+                if(documentSnapshot.contains("image_cover")){
+                    String imageCover = documentSnapshot.getString("image_cover");
+                    if(imageCover != null){
+                        if(!imageCover.isEmpty()){
+                            Picasso.with(getContext()).load(imageCover).into(mImageCover);
+                        }
+                    }
+                }
+            }
+
+        }
+    });
    }
 }
--- app/src/main/java/com/loschidos/chatgram/providers/PostProvider.java
+++ app/src/main/java/com/loschidos/chatgram/providers/PostProvider.java
@@ -21,4 +21,8 @@ public class PostProvider {
     public Query getAll(){
         return mCollection.orderBy("titulo", Query.Direction.DESCENDING);
     }
+
+    public Query getPostByUser(String id) {
+        return mCollection.whereEqualTo("idUsuario",id);
+    }
 }
